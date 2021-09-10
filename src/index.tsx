fcc518c347a924325638cf2890edb6dd8d4fe5bd
SungSeokMin <jkl154527@gmail.com>
2021-09-10 15:50:40 +0900

    🎨 리액트 - 리덕스 연결

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import Thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, applyMiddleware(Thunk));
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );

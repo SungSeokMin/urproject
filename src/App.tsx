4d11047ba2dbbc7590f89e619db32ededf2c5a69
SungSeokMin <jkl154527@gmail.com>
2021-09-10 15:50:24 +0900

    🎨 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Board from './pages/Board';
+import Main from './pages/Main';
+import Header from './pages/Header';
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={Main} exact />
+        <Route path="/board" component={Board} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;

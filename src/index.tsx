b829c2fe4c03c2d34754ac11fed79444e36e9c47
SungSeokMin <jkl154527@gmail.com>
2021-09-13 12:14:11 +0900

    ⚡️ history 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import Thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { createBrowserHistory } from 'history';
+
+export const history = createBrowserHistory();
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(Thunk))
+  composeWithDevTools(applyMiddleware(Thunk.withExtraArgument({ history })))
 );
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router history={history}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );

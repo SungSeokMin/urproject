fc1e2667d13d05d76bdd0f49672543dee5b6b7bd
SungSeokMin <jkl154527@gmail.com>
2021-09-14 14:40:57 +0900

    💄 Header 디자인

--- src/containers/HeaderContainer.tsx
+++ src/containers/HeaderContainer.tsx
@@ -8,6 +8,7 @@ import { userLoginAsync } from '../modules/thunk/userThunks';
 import { logout } from '../modules/user';
 import LoginModal from '../pages/modal/LoginModal';
 import SignUpModal from '../pages/modal/SignUpModal';
+import styles from '../css/Header.module.css';
 
 type HeaderContainerProps = RouteComponentProps;
 
@@ -45,28 +46,34 @@ function HeaderContainer({ history }: HeaderContainerProps) {
 
   return (
     <>
-      <header>
-        <div className="logo-container">
+      <header className={styles.header}>
+        <div className={styles.logoContainer}>
           <Link to="/">
             <img src="/logo.png" alt="로고" />
           </Link>
         </div>
-        <div className="signIn-signUp">
+
+        <div className={styles.menuContainer}>
+          <Link to="/post" className={styles.projectLink}>
+            <AiOutlineFundProjectionScreen />
+            프로젝트
+          </Link>
+        </div>
+
+        <div className="user-conatiner">
           {isLogin ? (
-            <button className="signIn" onClick={handleLogout}>
+            <button className={styles.signOutBtn} onClick={handleLogout}>
               로그아웃
             </button>
           ) : (
-            <button className="signIn" onClick={showLoginModal}>
+            <button className={styles.loginBtn} onClick={showLoginModal}>
               로그인
             </button>
           )}
-          {!isLogin && <button onClick={showSignUpModal}>회원가입</button>}
-          {isLogin && (
-            <Link to="/post/">
-              <AiOutlineFundProjectionScreen />
-              프로젝트
-            </Link>
+          {!isLogin && (
+            <button className={styles.signUpBtn} onClick={showSignUpModal}>
+              회원가입
+            </button>
           )}
         </div>
       </header>

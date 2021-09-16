c84fbb6609e91d1dbd1b1397287e464e843dc646
SungSeokMin <jkl154527@gmail.com>
2021-09-16 14:53:44 +0900

    💄 글 작성 버튼 디자인

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -10,38 +10,45 @@ type PostListProps = {
 
 function PostList({ posts }: PostListProps) {
   return (
-    <div className={styles.postListConatiner}>
-      {posts.map((post) => (
-        <Link to={`/post/${post.id}`} key={post.id}>
-          <div className={styles.postContainer}>
-            <div className={styles.imgAria}>
-              {post.thumbnail ? (
-                <img src={post.thumbnail} alt="썸네일" />
-              ) : (
-                <img
-                  src="https://urproject-image.s3.amazonaws.com/whiteBackground.png"
-                  alt="썸네일"
-                />
-              )}
-            </div>
-            <div className={styles.titleAria}>
-              <h3>
-                {post.title.length > 15
-                  ? `${post.title.slice(0, 15)}...`
-                  : post.title}
-              </h3>
-            </div>
-            <div className={styles.nicknameAndLike}>
-              <div className={styles.nickname}>{post.nickname}</div>
-              <div className={styles.like}>
-                <FaHeartbeat className={styles.likeIcon} />
-                <span>{post.like}</span>
+    <>
+      <div className={styles.writeBtn}>
+        <Link to="/write" className={styles.createPost}>
+          글 작성
+        </Link>
+      </div>
+      <div className={styles.postListConatiner}>
+        {posts.map((post) => (
+          <Link to={`/post/${post.id}`} key={post.id}>
+            <div className={styles.postContainer}>
+              <div className={styles.imgAria}>
+                {post.thumbnail ? (
+                  <img src={post.thumbnail} alt="썸네일" />
+                ) : (
+                  <img
+                    src="https://urproject-image.s3.amazonaws.com/whiteBackground.png"
+                    alt="썸네일"
+                  />
+                )}
+              </div>
+              <div className={styles.titleAria}>
+                <h3>
+                  {post.title.length > 15
+                    ? `${post.title.slice(0, 15)}...`
+                    : post.title}
+                </h3>
+              </div>
+              <div className={styles.nicknameAndLike}>
+                <div className={styles.nickname}>{post.nickname}</div>
+                <div className={styles.like}>
+                  <FaHeartbeat className={styles.likeIcon} />
+                  <span>{post.like}</span>
+                </div>
               </div>
             </div>
-          </div>
-        </Link>
-      ))}
-    </div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
 

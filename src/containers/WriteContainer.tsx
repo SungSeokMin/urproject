c31dac3b2adace435ea074d4f30883cefc4a03bf
SungSeokMin <jkl154527@gmail.com>
2021-09-17 10:57:24 +0900

    📝 nickname 불러오기

--- src/containers/WriteContainer.tsx
+++ src/containers/WriteContainer.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Write from '../components/Write';
+import { RootState } from '../modules';
+import { RouteComponentProps, withRouter } from 'react-router';
 
-function WriteContainer() {
-  return <Write />;
+function WriteContainer(props: RouteComponentProps) {
+  const { nickname } = useSelector((state: RootState) => state.user);
+
+  return <Write nickname={nickname} />;
 }
 
-export default WriteContainer;
+export default withRouter(WriteContainer);

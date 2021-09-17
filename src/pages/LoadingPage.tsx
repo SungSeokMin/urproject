ce9c72ca7d8fd350a96a7bcc4a60192d57d4ef61
SungSeokMin <jkl154527@gmail.com>
2021-09-17 11:55:17 +0900

    💄 로딩 페이지 디자인

---
+++ src/pages/LoadingPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../css/LoadingPage.module.css';
+
+function LoadingPage() {
+  return (
+    <div className={styles.loading}>
+      <h2>Ur Project</h2>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
+
+export default LoadingPage;

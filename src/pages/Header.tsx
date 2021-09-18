d8f39113e1b1168af90acf9687d434f7b5b3e95b
SungSeokMin <jkl154527@gmail.com>
2021-09-18 15:14:07 +0900

    📝 로그인 상태관리 (modal -> app)

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import HeaderContainer from '../containers/HeaderContainer';
 
-function Header() {
-  return <HeaderContainer />;
+type HeaderProps = {
+  signIn: boolean;
+  signUp: boolean;
+  showLoginModal: () => void;
+  showSignUpModal: () => void;
+  notShow: () => void;
+};
+
+function Header({
+  signIn,
+  signUp,
+  showLoginModal,
+  showSignUpModal,
+  notShow,
+}: HeaderProps) {
+  return (
+    <HeaderContainer
+      signIn={signIn}
+      signUp={signUp}
+      showLoginModal={showLoginModal}
+      showSignUpModal={showSignUpModal}
+      notShow={notShow}
+    />
+  );
 }
 
 export default Header;

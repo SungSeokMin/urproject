f832048dbcffdfed17407f0803fc74e2843a3567
SungSeokMin <jkl154527@gmail.com>
2021-09-19 14:12:10 +0900

    🎨  footer

---
+++ src/pages/main/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../../css/Main.module.css';
+
+function Footer() {
+  return <div className={styles.footerContainer}></div>;
+}
+
+export default Footer;
